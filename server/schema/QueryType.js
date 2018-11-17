11020c428982a2e3ba0177ba2478323b60b30609
Daniel Nolan <DanNolan99@gmail.com>
2018-11-16 21:04:09 -0500

    query reuse

--- server/schema/QueryType.js
+++ server/schema/QueryType.js
@@ -11,85 +11,36 @@ const {
 const { MODEL_DB } = require('../config');
 const { dbResolver, dbReader } = require('./utils');
 
+const findOneModel = (type, db) => ({
+  type,
+  args: {
+    id: { type: GraphQLString }
+  },
+  resolve: (_, {id}) => dbResolver(db, id),
+});
+
+const findManyModel = (type, db) => ({
+    type: new GraphQLList(type),
+    args: {
+      containsId: { type: new GraphQLList(GraphQLString) }
+    },
+    resolve: async (_, { containsId }) => {
+      const ids = containsId || await dbReader(db);
+      return Promise.all(ids.map(id => dbResolver(db, id)));
+    }
+})
+
 const QueryType = new GraphQLObjectType({
   name: 'Query',
   fields: {
-    stage: {
-      type: StageType,
-      args: {
-        id: { type: GraphQLString }
-      },
-      resolve: function (_, {id}) {
-        return dbResolver(MODEL_DB.STAGES, id)
-      }
-    },
-    stages: {
-      type: new GraphQLList(StageType),
-      args: {
-        containsId: { type: new GraphQLList(GraphQLString) }
-      },
-      resolve: async (_, { containsId }) => {
-        const ids = containsId || await dbReader(MODEL_DB.STAGES);
-        return Promise.all(ids.map(id => dbResolver(MODEL_DB.STAGES, id)));
-      }
-    },
-    stageContainerGroup: {
-      type: StageContainerGroupType,
-      args: {
-        id: { type: GraphQLString }
-      },
-      resolve: function (_, {id}) {
-        return dbResolver(MODEL_DB.STAGE_CONTAINER_GROUPS, id)
-      }
-    },
-    stageContainerGroups: {
-      type: new GraphQLList(StageContainerGroupType),
-      args: {
-        containsId: { type: new GraphQLList(GraphQLString) }
-      },
-      resolve: async (_, { containsId }) => {
-        const ids = containsId || (await dbReader(MODEL_DB.STAGE_CONTAINER_GROUPS));
-        return Promise.all(ids.map(id => dbResolver(MODEL_DB.STAGE_CONTAINER_GROUPS, id)));
-      }
-    },
-    stageContainer: {
-      type: StageContainerType,
-      args: {
-        id: { type: GraphQLString }
-      },
-      resolve: function (_, {id}) {
-        return dbResolver(MODEL_DB.STAGE_CONTAINERS, id)
-      }
-    },
-    stageContainers: {
-      type: new GraphQLList(StageContainerType),
-      args: {
-        containsId: { type: new GraphQLList(GraphQLString) }
-      },
-      resolve: async (_, { containsId }) => {
-        const ids = containsId || (await dbReader(MODEL_DB.STAGE_CONTAINERS));
-        return Promise.all(ids.map(id => dbResolver(MODEL_DB.STAGE_CONTAINERS, id)));
-      }
-    },
-    codeFile: {
-      type: CodeFileType,
-      args: {
-        id: { type: GraphQLString }
-      },
-      resolve: function (_, {id}) {
-        return dbResolver(MODEL_DB.CODE_FILES, id)
-      }
-    },
-    codeFiles: {
-      type: new GraphQLList(CodeFileType),
-      args: {
-        containsId: { type: new GraphQLList(GraphQLString) }
-      },
-      resolve: async (_, { containsId }) => {
-        const ids = containsId || await dbReader(MODEL_DB.CODE_FILES);
-        return Promise.all(ids.map(id => dbResolver(MODEL_DB.CODE_FILES, id)));
-      }
-    }
+    stage: findOneModel(StageType, MODEL_DB.STAGES),
+    stages: findManyModel(StageType, MODEL_DB.STAGES),
+    stageContainerGroup: findOneModel(StageContainerGroupType, MODEL_DB.STAGE_CONTAINER_GROUPS),
+    stageContainerGroups: findManyModel(StageContainerGroupType, MODEL_DB.STAGE_CONTAINER_GROUPS),
+    stageContainer: findOneModel(StageContainerType, MODEL_DB.STAGE_CONTAINERS),
+    stageContainers: findManyModel(StageContainerType, MODEL_DB.STAGE_CONTAINERS),
+    codeFile: findOneModel(CodeFileType, MODEL_DB.CODE_FILES),
+    codeFiles: findManyModel(CodeFileType, MODEL_DB.CODE_FILES),
   }
 });
 
