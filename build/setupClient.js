1a1e29ddb56aee14804d0aee9d178bd04a30a361
Daniel Nolan <DanNolan99@gmail.com>
2019-01-13 15:08:23 -0500

    shows client && server output

--- build/setupClient.js
+++ build/setupClient.js
@@ -1,11 +1,18 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 function setupClient(cwd) {
   return new Promise((resolve, reject) => {
-    const child = exec('npm i && npm run build', { cwd });
-    child.on('exit', (code) => {
-      if(code === 0) resolve();
-      else reject('Failed to build client');
+    const installChild = spawn('npm', ['i'], { cwd, stdio: 'inherit' });
+    installChild.on('exit', (code) => {
+      const runChild = spawn('npm', ['run', 'build'], { cwd, stdio: 'inherit' });
+      if(code !== 0) {
+        reject('Failed to install client dependencies!');
+        return;
+      }
+      runChild.on('exit', (code) => {
+        if(code === 0) resolve();
+        else reject('Failed to build client!');
+      });
     });
   });
 }
--- build/setupServer.js
+++ build/setupServer.js
@@ -1,8 +1,8 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 function setupServer(cwd) {
   return new Promise((resolve, reject) => {
-    const child = exec('npm i', { cwd });
+    const child = spawn('npm', ['i'], { cwd, stdio: 'inherit' });
     child.on('exit', (code) => {
       if(code === 0) resolve();
       else reject('Failed to build server');
