e3c0a09b8b91777dcda1b00b0c742db00234e4f8
Daniel Nolan <DanNolan99@gmail.com>
2019-01-16 21:21:58 -0500

    Fix Broken Stage Deletion

--- server/test/mutations/stages/destroyStageTest.js
+++ server/test/mutations/stages/destroyStageTest.js
@@ -16,21 +16,45 @@ const {
 } = require('../util');
 const destroyStage = mutationWrapper(require('../../../src/schema/mutations/stage/destroy'));
 
+const existingCodeFile = {
+  id: 4,
+  initialCode: LOOKUP_KEY,
+  codeStageIds: [1],
+}
+const existingSolutions = [
+  { id: 2, codeFileId: existingCodeFile.id, },
+  { id: 3, codeFileId: existingCodeFile.id, },
+]
 const existingStage = {
   id: 1,
   task: LOOKUP_KEY,
   abiValidations: LOOKUP_KEY,
   details: LOOKUP_KEY,
+  codeFileIds: [existingCodeFile.id]
 }
 
 mockSuite('Mutations::Stages::Destroy', () => {
   before(async () => {
+    existingSolutions.forEach((solution) => {
+      mockConfigDocument(MODEL_DB.SOLUTIONS, solution);
+    });
+    mockConfigDocument(MODEL_DB.CODE_FILES, existingCodeFile);
     mockConfigDocument(MODEL_DB.STAGES, existingStage);
     await destroyStage(existingStage.id);
   });
 
   it('should have removed the stage', () => {
     assert(removedModels.stages[existingStage.id]);
+  });
+
+  it('should have removed the codeFile', () => {
+    assert(removedModels.codeFiles[existingCodeFile.id]);
+  });
+
+  it('should have removed the solutions', () => {
+    existingSolutions.forEach(solution => {
+      assert(removedModels.solutions[solution.id]);
+    });
   });
 
   it('should have removed the task project path', () => {
