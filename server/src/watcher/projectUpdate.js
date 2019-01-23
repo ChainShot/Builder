880c0b08aebd76ad9be936d1a625d147624160c5
Daniel Nolan <DanNolan99@gmail.com>
2019-01-23 18:28:46 -0500

    Remove unnecessary files

--- server/src/watcher/projectUpdate.js
+++
@@ -1,9 +0,0 @@
-const { PROJECTS_DIR } = require('../config');
-const stageContainerLookup = require('./stageContainerLookup');
-
-const projectUpdate = (name) => {
-  const fileName = name.replace(`${PROJECTS_DIR}/`, '');
-  return stageContainerLookup(fileName);
-}
-
-module.exports = projectUpdate;
