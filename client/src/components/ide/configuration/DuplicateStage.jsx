02679863838affbc0ec0b97375d85e54bef25fe9
Daniel Nolan <DanNolan99@gmail.com>
2019-01-23 15:11:35 -0500

    Duplicate Stage Options: Create new CodeFiles or Link Existing

--- client/src/components/ide/configuration/DuplicateStage.jsx
+++ client/src/components/ide/configuration/DuplicateStage.jsx
@@ -2,24 +2,42 @@ import React, { Component } from 'react';
 import Dialog from '../../Dialog';
 import { close } from '../../../utils/dialog';
 import apiMutation from '../../../utils/api/mutation';
+import StyledCheckbox from '../../forms/StyledCheckbox';
+import StyledInput from '../../forms/StyledInput';
 
 const variables = [
   ['id', 'String'],
   ['title', 'String'],
   ['position', 'Int'],
+  ['createNew', 'Boolean'],
 ]
 
 const args = variables.map(([prop, type]) => `$${prop}: ${type}`).join(', ');
 const mapping = variables.map(([prop, type]) => `${prop}: $${prop}`).join(', ');
-const returns = variables.map(([prop]) => `${prop}`).join('\n    ');
 
 const mutation = `
 mutation duplicateStage(${args}) {
   duplicateStage(${mapping}) {
-    ${returns}
+    id
   }
 }
 `;
+
+const TITLE_HINT = 'Short name displayed to the user';
+const POSITION_HINT = 'The order of this stage relative to other stages';
+const LINKED_CODE_FILE_HINT = "If checked this will create new versions of the code files rather than the existing ones"
+
+const validators = {
+  title: (x) => !!x,
+  position: (x) => x >= 0,
+}
+
+const validate = (props) => {
+  return Object.keys(props).reduce((errors, prop) => {
+    if(validators.hasOwnProperty(prop) && !validators[prop](props[prop])) return errors.concat(prop);
+    return errors;
+  }, []);
+}
 
 class DuplicateStage extends Component {
   constructor(props) {
@@ -27,44 +45,92 @@ class DuplicateStage extends Component {
     this.state = {
       title: `${props.title} (copy)`,
       position: props.position + 1,
+      createNew: false,
+      errors: [],
     }
   }
   onSubmit = async (evt) => {
     evt && evt.preventDefault();
+    const { title, position, createNew, errors } = this.state;
+    if(errors.length > 0) return;
     const { id } = this.props;
-    const { title, position } = this.state;
-    apiMutation(mutation, { id, title, position }).then(({ id }) => {
+    apiMutation(mutation, { id, title, position, createNew }).then(({ id }) => {
       close(id);
     });
   }
+  validate() {
+    const { title, position } = this.state;
+    const errors = validate({ title, position });
+    this.setState({ errors });
+  }
+  componentDidMount() {
+    this.validate();
+  }
   handleChange(prop, value) {
-    this.setState({[prop]: value});
+    this.setState({ [prop]: value }, this.validate);
+  }
+  renderOption() {
+    const { codeFileIds } = this.props;
+    const { createNew } = this.state;
+    if(codeFileIds && codeFileIds.length > 0) {
+      return (
+        <StyledCheckbox
+          onChange={(createNew) => this.handleChange('createNew', createNew)}
+          hint={LINKED_CODE_FILE_HINT}
+          label="Re-Create Code Files?"
+          checked={createNew} />
+      )
+    }
+    return null;
   }
   render() {
-    const { title, position } = this.state;
+    const { title, position, errors } = this.state;
     return (
       <Dialog title="Duplicate Stage" className="duplicate-stage">
         <form onSubmit={this.onSubmit}>
-          <label>
-            <span>Title</span>
-            <input type="text" className="styled" value={title}
-              onChange={({ target }) => this.handleChange('title', target.value)}/>
-          </label>
+          <StyledInput
+            label="Title"
+            hint={TITLE_HINT}
+            type="text"
+            errors={errors}
+            field="title"
+            value={title}
+            onChange={({ target }) => this.handleChange('title', target.value)} />
 
-          <label>
-            <span>Position</span>
-            <input type="number" className="styled" value={position}
-              onChange={({ target }) => this.handleChange('position', +target.value)}/>
-          </label>
+          <StyledInput
+            label="Position"
+            hint={POSITION_HINT}
+            type="number"
+            errors={errors}
+            field="position"
+            value={position}
+            onChange={({ target }) => this.handleChange('position', +target.value)} />
 
-          <div className="actions">
-            <div className="btn btn-primary" onClick={this.onSubmit}>
-              Create Duplicate
-            </div>
-          </div>
+          {this.renderOption()}
+
+          <Actions
+            onSubmit={this.onSubmit}
+            errors={errors} />
         </form>
       </Dialog>
     );
+  }
+}
+
+class Actions extends Component {
+  render() {
+    const { onSubmit, errors } = this.props;
+    const submitClasses = ['btn btn-primary'];
+    if(errors.length > 0) {
+      submitClasses.push('disabled')
+    }
+    return (
+      <div className="actions">
+        <div className={submitClasses.join(' ')} onClick={onSubmit}>
+          Create Duplicate
+        </div>
+      </div>
+    )
   }
 }
 
