c60f0ec4e77af51f265e9c896b4a979c19135d19
Daniel Nolan <DanNolan99@gmail.com>
2019-01-25 20:53:15 -0500

    Allows for opening Multiple Stages

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -3,10 +3,12 @@ import saveState from "./saveState";
 import executionState from "./executionState";
 import compilationState from "./compilationState";
 import codeFilePaneState from './codeFilePaneState';
+import sidebarState from './sidebarState';
 
 export default combineReducers({
   saveState,
   executionState,
   compilationState,
-  codeFilePaneState
+  codeFilePaneState,
+  sidebarState,
 });
