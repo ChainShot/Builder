e5d0d3ffe625a081f9753a9f37fe9f5431919e03
Daniel Nolan <DanNolan99@gmail.com>
2019-02-27 13:11:59 -0500

    action nav for skeletonnav

--- client/src/components/ide/sidebar/SkeletonNav.jsx
+++ client/src/components/ide/sidebar/SkeletonNav.jsx
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
 import SVG from 'components/SVG';
-import { openTab } from 'redux/actions';
-import { connect } from 'react-redux';
 import { IDE_TAB_TYPES } from 'config';
+import ActionNav from './ActionNav';
 
 class SkeletonNav extends Component {
-  openTab = () => {
-    const { skeleton, stage } = this.props;
-    this.props.openTab(stage.id, IDE_TAB_TYPES.SKELETON_CONFIG, skeleton.id);
-  }
   render() {
-    const { skeleton } = this.props;
+    const { skeleton, stage } = this.props;
     const { title, id } = skeleton;
+    const attrs = {
+      stageId: stage.id,
+      type: IDE_TAB_TYPES.SKELETON_CONFIG,
+      id,
+    }
     return (
-      <li>
-        <div className="action" onClick={this.openTab}>
-          <SVG name="skeleton"/>
-          <span>{ title }</span>
-        </div>
-      </li>
+      <ActionNav attrs={attrs}>
+        <SVG name="skeleton"/>
+        <span>{ title }</span>
+      </ActionNav>
     )
   }
 }
 
-const mapDispatchToProps = { openTab } 
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(SkeletonNav);
+export default SkeletonNav;
