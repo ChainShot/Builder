9bf06708547cc7d3cb9d0ab72551d3e8189b8f8e
Daniel Nolan <DanNolan99@gmail.com>
2019-04-05 18:22:34 -0700

    Resizable Code File Pane

--- client/src/components/ide/codeFile/panes/CodeFilePane.jsx
+++ client/src/components/ide/codeFile/panes/CodeFilePane.jsx
@@ -2,12 +2,52 @@ import React, { Component } from 'react';
 import Output from './Output';
 import Compilation from './Compilation';
 import { CODE_FILE_PANES } from 'config';
+import Resizable from 're-resizable';
+import "./CodeFilePane.scss";
+
+const LOCALSTORAGE_PANE = 'ui-settings:code-file-pane';
+const DEFAULT_SIZE = { width: '100%', height: 300 }
+const MIN_HEIGHT = 200;
+const MAX_HEIGHT = 600;
+const DIRECTIONS = { top:true, right:false, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }
+
+function setPaneSize(height, width = "100%") {
+  localStorage.setItem(LOCALSTORAGE_PANE, JSON.stringify({
+    width,
+    height,
+  }));
+}
+
+function getPaneSize() {
+  let data = localStorage.getItem(LOCALSTORAGE_PANE);
+  try {
+    if(data) return JSON.parse(data);
+  }
+  catch(ex) {
+    console.warn(`Could not parse ${data}`);
+  }
+}
 
 class CodeFilePane extends Component {
+  onResizeStop(evt, dir, ref) {
+    setPaneSize(ref.style.height);
+    // resize event for the monaco display
+    window.requestAnimationFrame(() => window.dispatchEvent(new CustomEvent('resize')));
+  }
   render() {
     const { pane, stage, codeFile, changePane, code } = this.props;
+    const defaultSize = getPaneSize() || DEFAULT_SIZE;
+    if([CODE_FILE_PANES.OUTPUT_TAB, CODE_FILE_PANES.COMPILATION_TAB].indexOf(pane) < 0) {
+      return null;
+    }
     return (
-      <React.Fragment>
+      <Resizable 
+        class="code-file-pane"
+        defaultSize={defaultSize}
+        enable={DIRECTIONS}
+        onResizeStop={this.onResizeStop}
+        minHeight={MIN_HEIGHT}
+        maxHeight={MAX_HEIGHT}>
         <Output stage={stage}
                 code={code}
                 codeFile={codeFile}
@@ -18,7 +58,7 @@ class CodeFilePane extends Component {
                 codeFile={codeFile}
                 shouldShow={pane === CODE_FILE_PANES.COMPILATION_TAB}
                 hide={changePane}/>
-      </React.Fragment>
+      </Resizable>
     )
   }
 }
