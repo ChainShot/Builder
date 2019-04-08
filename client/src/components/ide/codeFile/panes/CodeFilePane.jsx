5bf93786b3dd01c148623e3122b72fba459f15af
Daniel Nolan <DanNolan99@gmail.com>
2019-04-08 10:36:05 -0700

    fix resize bug

--- client/src/components/ide/codeFile/panes/CodeFilePane.jsx
+++ client/src/components/ide/codeFile/panes/CodeFilePane.jsx
@@ -29,35 +29,60 @@ function getPaneSize() {
 }
 
 class CodeFilePane extends Component {
-  onResizeStop(evt, dir, ref) {
-    setPaneSize(ref.style.height);
+  constructor(props) {
+    super(props);
+    const defaultSize = getPaneSize() || DEFAULT_SIZE;
+    console.log({ defaultSize })
+    this.state = {
+      size: defaultSize
+    }
+  }
+  onResizeStop = (evt, dir, ref, delta) => {
+    const { height, width } = this.state.size;
+    const newHeight = height + delta.height;
+    // don't store height values when hidden (below min height)
+    if(newHeight >= MIN_HEIGHT) {
+      console.log('setting', newHeight)
+      setPaneSize(newHeight);
+      this.setState({ size: { width, height: newHeight }});
+    }
+    // resize event for the monaco display
+    window.requestAnimationFrame(() => window.dispatchEvent(new CustomEvent('resize')));
+  }
+  hide() {
+    const { changePane } = this.props;
+    changePane();
     // resize event for the monaco display
     window.requestAnimationFrame(() => window.dispatchEvent(new CustomEvent('resize')));
   }
   render() {
     const { pane, stage, codeFile, changePane, code } = this.props;
-    const defaultSize = getPaneSize() || DEFAULT_SIZE;
+    const { size } = this.state;
+    let minHeight = MIN_HEIGHT;
+    let height = size.height;
+    const width = size.width;
     if([CODE_FILE_PANES.OUTPUT_TAB, CODE_FILE_PANES.COMPILATION_TAB].indexOf(pane) < 0) {
-      return null;
+      height = 0;
+      minHeight = 0;
     }
     return (
       <Resizable 
-        class="code-file-pane"
-        defaultSize={defaultSize}
+        className='code-file-pane'
+        size={{ height, width }}
         enable={DIRECTIONS}
         onResizeStop={this.onResizeStop}
-        minHeight={MIN_HEIGHT}
+        minHeight={minHeight}
         maxHeight={MAX_HEIGHT}>
         <Output stage={stage}
                 code={code}
                 codeFile={codeFile}
                 shouldShow={pane === CODE_FILE_PANES.OUTPUT_TAB}
-                hide={changePane}/>
+                hide={this.hide}/>
         <Compilation stage={stage}
                 code={code}
                 codeFile={codeFile}
                 shouldShow={pane === CODE_FILE_PANES.COMPILATION_TAB}
-                hide={changePane}/>
+                hide={this.hide}/>
       </Resizable>
     )
   }
