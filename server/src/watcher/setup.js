0bb82fc953533e743977ea6c6de6b1faba80fb35
Daniel Nolan <DanNolan99@gmail.com>
2020-02-13 01:21:28 -0500

    Split out Content Folders

--- server/src/watcher/setup.js
+++ server/src/watcher/setup.js
@@ -2,6 +2,8 @@ const { CONTENT_DIR } = require('../config');
 const watch = require('node-watch');
 const fs = require('fs-extra');
 
+const CONTENT_FOLDERS = ['projects', 'config'];
+
 function getClients(io) {
   return new Promise((resolve, reject) => {
     io.clients((err, clients) => {
@@ -11,12 +13,19 @@ function getClients(io) {
   });
 }
 
-const setup = (io) => {
-  watch(CONTENT_DIR, { recursive: true }, async () => {
+function watchDir(dir) {
+  watch(dir, { recursive: true }, async () => {
     const clients = await getClients(io);
     if(clients.length > 0) {
       io.sockets.emit('update');
     }
+  });
+}
+
+const setup = (io) => {
+  CONTENT_FOLDERS.forEach((folderName) => {
+    const fullDir = path.join(CONTENT_DIR, folderName);
+    watchDir(fullDir);
   });
 }
 
