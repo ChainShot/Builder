36bdcac642ef27530edf62d35401f10ef834bd34
Daniel Nolan <DanNolan99@gmail.com>
2020-11-30 16:29:11 -0500

    Adds Runner Server Key Config

--- client/src/utils/api/runner.js
+++ client/src/utils/api/runner.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
-import {RUNNER_URL} from '../../config';
+import {RUNNER_URL, RUNNER_SERVER_KEY} from '../../config';
 
 export default axios.create({
     baseURL: RUNNER_URL,
+    transformRequest: [(data, headers) => {
+        headers['authorization'] = RUNNER_SERVER_KEY;
+        return JSON.stringify(data);
+    }],
+    // transform request is setting it to form data,
+    // seeing similar issues as reported here:
+    // https://github.com/axios/axios/issues/860
+    headers: {
+        'Content-Type': 'application/json'
+    },
     transformResponse: [(response, headers) => {
         const contentType = headers['content-type'];
         if (contentType && contentType.indexOf('json') >= 0) {
